70fb4cdd2e52b47c85faa6a50751c2334465ee32
kimjames791 <kimjames791@gmail.com>
2024-03-19 11:23:58 -0700

    lists.h

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -3,7 +3,7 @@
 
 /**
  * struct listint_s - singly linked list
- * @k: integer
+ * @n: integer
  * @next: points to the next node
  *
  * Description: singly linked list node structure
@@ -11,12 +11,12 @@
  */
 typedef struct listint_s
 {
-    int k;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
-listint_t *add_nodeint_end(listint_t **head, const int k);
+listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int is_palindrome(listint_t **head);
