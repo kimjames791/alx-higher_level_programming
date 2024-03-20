7563acea279d929c9371e881a53f138386b4c6eb
kimjames791 <kimjames791@gmail.com>
2024-03-20 12:11:46 -0700

    11-multiply_list_map.py

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-def mutiply_list_map(my_list=[], number=0):
+def mutiply_list_map(my_list=None, number=0):
+    if my_list is None:
+        return []
     return list(map(lambda x: x * number, my_list))
+
