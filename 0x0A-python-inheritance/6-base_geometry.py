86d40ffdb7020655957cca115f2de5bd7da83e7b
kimjames791 <kimjames791@gmail.com>
2024-04-15 10:30:21 -0700

    6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This func Contains the class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """contains a  class with public attribute area"""
+    def area(self):
+        """raises an exception when called"""
+        raise Exception("area() is not implemented in the func")
